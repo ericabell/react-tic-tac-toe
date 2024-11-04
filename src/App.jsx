a1950d95bf9eefd2cb1b1ee4f993ecaac5364f8e
Eric Abell <ericabell@users.noreply.github.com>
2024-11-04 16:36:20 -0600

    create Game top-level component, stores history

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,7 @@ function Square({ value, onSquareClick }) {
   </button>
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -23,10 +20,8 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
-
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -57,6 +52,28 @@ export default function Board() {
   )
 }
 
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+      <div className="game">
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+        </div>
+        <div className="game-info">
+          <ol>{/*TODO*/}</ol>
+        </div>
+      </div>
+    );
+}
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -68,6 +85,7 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6]
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
