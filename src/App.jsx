2d8b009ac356931f3fae69accc25d35081c72885
Eric Abell <ericabell@users.noreply.github.com>
2024-11-04 14:23:59 -0600

    create new component Square

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
+function Square() {
+  return <button class="w-24 p-6 border border-indigo-600 rounded-none">1</button>
+}
+
 export default function Board() {
 
   return (
     <>
     <div>
-      <button class="w-24 p-6 border border-indigo-600 rounded-none">1</button>
-      <button class="w-24 p-6 border border-indigo-600 rounded-none">2</button>
-      <button class="w-24 p-6 border border-indigo-600 rounded-none">3</button>
+      <Square />
+      <Square />
+      <Square />
     </div>
     <div>
-      <button class="w-24 p-6 border border-indigo-600 rounded-none">4</button>
-      <button class="w-24 p-6 border border-indigo-600 rounded-none">5</button>
-      <button class="w-24 p-6 border border-indigo-600 rounded-none">6</button>
+      <Square />
+      <Square />
+      <Square />
     </div>
     <div>
-      <button class="w-24 p-6 border border-indigo-600 rounded-none">7</button>
-      <button class="w-24 p-6 border border-indigo-600 rounded-none">8</button>
-      <button class="w-24 p-6 border border-indigo-600 rounded-none">9</button>
+      <Square />
+      <Square />
+      <Square />      
     </div>
     </>
   )
