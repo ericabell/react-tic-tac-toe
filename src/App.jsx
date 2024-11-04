f88d5de57b493dec85e360da8b195a84e521bb5f
Eric Abell <ericabell@users.noreply.github.com>
2024-11-04 14:27:42 -0600

    add prop value for Square

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-function Square() {
-  return <button class="w-24 p-6 border border-indigo-600 rounded-none">1</button>
+function Square({ value }) {
+  return <button class="w-24 p-6 border border-indigo-600 rounded-none">{value}</button>
 }
 
 export default function Board() {
@@ -7,19 +7,19 @@ export default function Board() {
   return (
     <>
     <div>
-      <Square />
-      <Square />
-      <Square />
+      <Square value="1" />
+      <Square value="2"/>
+      <Square value="3"/>
     </div>
     <div>
-      <Square />
-      <Square />
-      <Square />
+      <Square value="4"/>
+      <Square value="5"/>
+      <Square value="6"/>
     </div>
     <div>
-      <Square />
-      <Square />
-      <Square />      
+      <Square value="7"/>
+      <Square value="8"/>
+      <Square value="9"/>      
     </div>
     </>
   )
