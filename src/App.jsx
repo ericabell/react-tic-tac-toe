9c16106cac75ba918d5853aee06e978cbd3371bb
Eric Abell <ericabell@users.noreply.github.com>
2024-11-04 15:05:22 -0600

    move state of board to Board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,32 @@
 import { useState } from 'react';
 
-function Square() {
-  const [value, setValue] = useState("");
-
-  function handleClick() {
-    setValue('X');
-  }
-
+function Square({ value }) {
   return <button
     className="w-24 p-6 border border-indigo-600 rounded-none"
-    onClick={handleClick}
     >
       {value}
   </button>
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
   return (
     <>
     <div>
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[0]}/>
+      <Square value={squares[1]}/>
+      <Square value={squares[2]}/>
     </div>
     <div>
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[3]}/>
+      <Square value={squares[4]}/>
+      <Square value={squares[5]}/>
     </div>
     <div>
-      <Square />
-      <Square />
-      <Square />      
+      <Square value={squares[6]}/>
+      <Square value={squares[7]}/>
+      <Square value={squares[8]}/>      
     </div>
     </>
   )
